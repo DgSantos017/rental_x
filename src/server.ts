73181946de84ff9eee6a45fd4aa7d0de1d5d5370
Diogo <diogosantosferreira.01@gmail.com>
2022-05-12 12:05:48 -0300

    feat: first router

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
+
 app.use(express.json())
-const array = []
-
-app.post('/teste', (req, res) => {
-	const { name, sobrename, age } = req.body
-	const obj = { name, sobrename, age }
-	array.push(obj)
-	res.status(201).json(obj)
-})
-
-app.get('/teste', (req, res) => {
-	res.json(array)
-})
+app.use('/categories', categoriesRoutes)
 
 app.listen(3000, () => {
 	console.log('running in port 3000')
