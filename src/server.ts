4712f266403ff1b13662dbe19c8610d90be5895f
Diogo <diogosantosferreira.01@gmail.com>
2022-05-12 12:04:15 -0300

    feat: first route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+const array = []
+
+app.post('/teste', (req, res) => {
+	const { name, sobrename, age } = req.body
+	const obj = { name, sobrename, age }
+	array.push(obj)
+	res.status(201).json(obj)
+})
+
+app.get('/teste', (req, res) => {
+	res.json(array)
+})
 
 app.listen(3000, () => {
 	console.log('running in port 3000')
