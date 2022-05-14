069456c5e4ad1cce6abebc4114f9d8fd1afd2372
Diogo <diogosantosferreira.01@gmail.com>
2022-05-14 00:18:56 -0300

    feat: create and list specifications

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository'
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
+
+const specificationsRepository = new SpecificationsRepository()
+
+const specificationsRoutes = Router()
+
+specificationsRoutes.post('/', (req, res) => {
+
+	const { name, description } = req.body
+
+	const createSpecificationService = new CreateSpecificationService(specificationsRepository)
+	createSpecificationService.execute({ name, description })
+
+	return res.status(201).send()
+
+})
+
+specificationsRoutes.get('/', (req, res) => {
+	const specifications = specificationsRepository.list()
+	return res.status(200).json(specifications)
+})
+
+export { specificationsRoutes }
