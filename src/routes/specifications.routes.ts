35db92318d2fd650020bf0b438c7a68e9a056670
Diogo <diogosantosferreira.01@gmail.com>
2022-05-14 17:08:07 -0300

    refactore: router specification method POST

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,26 +1,11 @@
 import { Router } from 'express'
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository'
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
-
-
-const specificationsRepository = new SpecificationsRepository()
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
 
 const specificationsRoutes = Router()
 
 specificationsRoutes.post('/', (req, res) => {
-
-	const { name, description } = req.body
-
-	const createSpecificationService = new CreateSpecificationService(specificationsRepository)
-	createSpecificationService.execute({ name, description })
-
-	return res.status(201).send()
-
+	return createSpecificationController.handle(req, res)
 })
 
-specificationsRoutes.get('/', (req, res) => {
-	const specifications = specificationsRepository.list()
-	return res.status(200).json(specifications)
-})
 
 export { specificationsRoutes }
