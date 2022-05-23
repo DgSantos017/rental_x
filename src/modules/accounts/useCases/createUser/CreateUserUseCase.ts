051facd88569572cce8d4fbcf3d22f4a438df3b2
Diogo <diogosantosferreira.01@gmail.com>
2022-05-23 19:01:37 -0300

    refactore: delete column username

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -2,7 +2,6 @@ import { inject, injectable } from 'tsyringe'
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO'
 import { IUsersRepository } from '../../repositpries/IUsersRepository'
 
-
 @injectable()
 class CreateUserUseCase {
 	constructor(
@@ -10,9 +9,9 @@ class CreateUserUseCase {
     private usersRepository: IUsersRepository
 	) {}
     
-	async execute({name, username, email, password, driver_license}: ICreateUserDTO):Promise<void>{
+	async execute({name, email, password, driver_license}: ICreateUserDTO):Promise<void>{
 		await this.usersRepository.create({
-			name, username, email, password, driver_license
+			name, email, password, driver_license
 		})
 	}
 }
