ba7c17967ae67ad008b64926c98d55eb1ddbf082
Diogo <diogosantosferreira.01@gmail.com>
2022-05-25 23:25:54 -0300

    feat: initialize tests by jest

--- src/modules/cars/useCases/listCategories/ListCategoryController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoryController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
-import { ListCategoriesUseCase } from './listCategoriesUseCase'
+import { ListCategoriesUseCase } from './ListCategoriesUseCase'
+
 
 class ListCategoriesController {
 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import multer from 'multer'
-import { CreateCategoryController } from '../modules/cars/useCases/createCategory/createCategoryController'
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController'
 import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController'
-import { ListCategoriesController } from '../modules/cars/useCases/listCategories/listCategoryController'
+import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoryController'
+
 
 
 const categoriesRoutes = Router()
