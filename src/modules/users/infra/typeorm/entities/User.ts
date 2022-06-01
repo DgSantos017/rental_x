3326ef908813083395b6a38f26542f8024ab0930
Diogo <diogosantosferreira.01@gmail.com>
2022-06-01 10:46:07 -0300

    feat: add migrations carr

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -3,28 +3,20 @@ import { v4 as uuid } from 'uuid'
 
 @Entity('users')
 class User {
-
   @PrimaryColumn()
   	id: string
-
   @Column()
   	name: string
-
   @Column()
   	email: string
-
   @Column()
   	password: string
-
   @Column()
   	driver_license: string
-
   @Column()
   	isAdmin: boolean
-
 	@Column()
 		avatar: string
-
   @CreateDateColumn()
   	created_at: Date
 
@@ -33,7 +25,6 @@ class User {
   		this.id = uuid()
   	}
   }
-
 }
 export { User }
 
