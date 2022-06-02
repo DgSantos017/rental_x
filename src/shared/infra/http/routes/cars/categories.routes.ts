1d46ca3bde74fc96ae5c3903188847a7165b6a16
Diogo <diogosantosferreira.01@gmail.com>
2022-06-02 00:40:12 -0300

    feat: add midleaware admin

--- src/shared/infra/http/routes/cars/categories.routes.ts
+++ src/shared/infra/http/routes/cars/categories.routes.ts
@@ -3,6 +3,10 @@ import multer from 'multer'
 import { CreateCategoryController } from '../../../../../modules/cars/useCases/createCategory/CreateCategoryController'
 import { ImportCategoryController } from '../../../../../modules/cars/useCases/importCategory/ImportCategoryController'
 import { ListCategoriesController } from '../../../../../modules/cars/useCases/listCategories/ListCategoryController'
+import { ensureAdmin } from '../../middlewares/ensureAdmin'
+import { ensureAuthenticated } from '../../middlewares/ensureAuthenticated'
+
+
 
 const categoriesRoutes = Router()
 
@@ -14,9 +18,9 @@ const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
 const listCategoriesController = new ListCategoriesController()
 
-categoriesRoutes.post('/', createCategoryController.handle)
+categoriesRoutes.post('/', ensureAuthenticated, ensureAdmin, createCategoryController.handle)
+categoriesRoutes.post('/import', ensureAuthenticated, ensureAdmin, upload.single('file'), importCategoryController.handle)
 categoriesRoutes.get('/', listCategoriesController.handle)
-categoriesRoutes.post('/import', upload.single('file'), importCategoryController.handle)
 
 export { categoriesRoutes }
 
